94bf4ace85473a2fec8ea1b6ff4c8835c988332d
narpat-curl <85334304+narpat-curl@users.noreply.github.com>
2025-04-18 03:51:17 +0530

    minor chnage

---
+++ init_db.py
@@ -0,0 +1,9 @@
+from database import Base, engine
+
+def init_db():
+    # Create all tables
+    Base.metadata.create_all(bind=engine)
+    print("Database tables created successfully!")
+
+if __name__ == "__main__":
+    init_db() 
